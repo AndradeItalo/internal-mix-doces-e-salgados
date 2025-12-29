973bddbfa80d2c5a0e7bebe18320d12073e75d50
AndradeItalo <ifa2@cin.ufpe.br>
2025-12-29 12:11:55 -0300

    wip - prod

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
       {
         source: "/api/:path*",
         destination: process.env.NODE_ENV === 'production' 
-          ? "https://mix-doces-internal.vercel.app/:path*"
+          ? "https://internal-mix-doces-e-salgados.vercel.app/:path*"
           : "http://localhost:4000/:path*",
       },
     ];
